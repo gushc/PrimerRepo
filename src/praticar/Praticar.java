23f5e57af36b55d0ca535aa4b385cf2681095077
Jose Gustavo <gustavo.-.josehc@hotmail.com>
2023-10-31 10:13:22 -0500

    1

---
+++ src/praticar/Praticar.java
@@ -0,0 +1,31 @@
+
+package praticar;
+
+import java.util.Scanner;
+
+
+public class Praticar {
+
+
+    public static void main(String[] args) {
+        
+        Double Respuesta;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Hola, estoy empezando de cero");
+        
+        System.out.println("Soy diferente");
+        System.out.println("Cuantos años tienes?");
+        
+            Respuesta = entrada.nextDouble();
+            
+            if(Respuesta <18  ){
+                System.out.println("Eres menor de edad");
+            }
+            else if(Respuesta >= 18){
+                System.out.println("Puedes seguir");
+            }
+        
+       
+        
+    }
+}
